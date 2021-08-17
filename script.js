78c36dfcf628e524e7c55d6ed7d11aa992b70f74
rafaelst2000 <rafaelst2000@gmail.com>
2021-08-17 14:24:02 -0300

    faq with accordion nav

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
-
-if(tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('ativo')
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
   
-  function activeTab(index) {
-    tabContent.forEach((content) => content.classList.remove('ativo'))
-    tabContent[index].classList.add('ativo')
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo')
+    
+    function activeTab(index) {
+      tabContent.forEach((content) => content.classList.remove('ativo'))
+      tabContent[index].classList.add('ativo')
+    }
+  
+    tabMenu.forEach((item, index) => item.addEventListener('click', () => activeTab(index)))
+  }
+}
+
+function InitAccordion() {
+  function activeAccordion() {
+    this.classList.toggle('ativo')
+    this.nextElementSibling.classList.toggle('ativo')
   }
   
-  tabMenu.forEach((item, index) => item.addEventListener('click', () => activeTab(index)))
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  if(accordionList.length) {
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+    accordionList.forEach((item) => item.addEventListener('click', activeAccordion))
+  }
 }
+
+initTabNav()
+InitAccordion()
+
