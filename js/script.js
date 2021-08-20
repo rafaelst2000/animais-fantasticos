5d1249c35ff068232789431554829e3a887f0543
rafaelst2000 <rafaelst2000@gmail.com>
2021-08-20 15:16:45 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimationScroll from './modules/animacao-scroll.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tab-nav.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initAnimationScroll()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
